32298626c29af0c74357f7a5d5eaba6e0cd0e957
Thomas Deinhamer <thasmo@gmail.com>
2016-08-10 22:41:02 +0200

    Install gosu and slim down the image size

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
+# Exit immediately on non-zero return codes.
 set -e
+
+# Use the `filebeat` command if only options were given.
+if [ "${1:0:1}" = '-' ]; then
+	set -- filebeat "$@"
+fi
+
+# Run as user `filebeat` if the command is `filebeat`.
+if [ "$1" = 'filebeat' ]; then
+	set -- gosu filebeat "$@"
+fi
+
+# Execute the command.
 exec "$@"
